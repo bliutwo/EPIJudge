50e432add424a9206bd8c9defdb6114d8e40fce5
bliutwo <bliutwo@cs.stanford.edu>
2021-03-13 21:20:19 -0800

    Revert "Add working solution 8.7"
    This reverts commit 9b2d205d7563cf3132749ad7bc2d8af1b4bf9ddc.

--- epi_judge_python/circular_queue.py
+++ epi_judge_python/circular_queue.py
@@ -6,31 +6,20 @@ class Queue:
     SCALE_FACTOR = 2
 
     def __init__(self, capacity: int) -> None:
-        self._entries = [0] * capacity
-        self._head = self._tail = self._num_queue_elements = 0
+        # TODO - you fill in here.
+        return
 
     def enqueue(self, x: int) -> None:
-        if self._num_queue_elements == len(self._entries): # Needs to resize.
-            # Makes the queue elements appear consecutively.
-            self._entries = (self._entries[self._head:] +
-                             self._entries[:self._head])
-            # Resets head and tail.
-            self._head, self._tail = 0, self._num_queue_elements
-            self._entries += [0] * (len(self._entries) * Queue.SCALE_FACTOR -
-                                    len(self._entries))
-
-        self._entries[self._tail] = x
-        self._tail = (self._tail + 1) % len(self._entries)
-        self._num_queue_elements += 1
+        # TODO - you fill in here.
+        return
 
     def dequeue(self) -> int:
-        self._num_queue_elements -= 1
-        result = self._entries[self._head]
-        self._head = (self._head + 1) % len(self._entries)
-        return result
+        # TODO - you fill in here.
+        return 0
 
     def size(self) -> int:
-        return self._num_queue_elements
+        # TODO - you fill in here.
+        return 0
 
 
 def queue_tester(ops):
