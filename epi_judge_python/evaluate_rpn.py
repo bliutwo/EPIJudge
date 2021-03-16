c6d76a7b5b9e3279a7c8d2cd8a0820a412786bcf
bliutwo <bliutwo@cs.stanford.edu>
2021-03-15 22:46:05 -0700

    Add nonworking 8.2 solution

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,37 @@
 from test_framework import generic_test
 
+def operation(a, b, operator):
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == 'x':
+        return a * b
+    elif operator == '/':
+        return a // b
+    else:
+        raise ValueError
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    s = expression.split(',')
+    operands = ['+', '-', 'x', '/']
+    curr_operator = None
+    a = None
+    b = None
+    top = None
+    while len(s) > 0:
+        top = s.pop()
+        if len(s) == 0:
+            break
+        if top in operands:
+            curr_operator = top
+        elif a == None and b == None:
+            a = top
+        elif a != None and b == None:
+            b = top
+        elif a and b and curr_operator:
+            s.append(operation(a, b, curr_operator))
+    return top
 
 
 if __name__ == '__main__':
