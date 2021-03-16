37ba8b9826d5138a0148bb24a98cb3045ae0e1b6
bliutwo <bliutwo@cs.stanford.edu>
2021-03-15 22:50:18 -0700

    Revert "Add textbook solution 8.2"
    This reverts commit e17b72cfb1608f0e8fcddf337f49b538335921cd.

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,20 +2,8 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    intermediate_results: List[int] = []
-    delimiter = ','
-    operators = {
-        '+': lambda y, x: x + y, '-': lambda y, x: x - y,
-        '*': lambda y, x: x * y, '/': lambda y, x: x // y
-    }
-
-    for token in expression.split(delimiter):
-        if token in operators:
-            intermediate_results.append(operators[token](
-                intermediate_results.pop(), intermediate_results.pop()))
-        else:   # token is a number
-            intermediate_results.append(int(token))
-    return intermediate_results[-1]
+    # TODO - you fill in here.
+    return 0
 
 
 if __name__ == '__main__':
