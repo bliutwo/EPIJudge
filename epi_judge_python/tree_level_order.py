202fdc407be58c7f58906ae52857f68e7b6795ac
bliutwo <bliutwo@cs.stanford.edu>
2021-03-15 22:12:11 -0700

    Add incomplete solution 8.6

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -2,11 +2,19 @@ from typing import List
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+import collections
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    bfs_q = collections.deque([tree])
+    curr_level = collections.deque([tree.data])
+    ans = []
+    while len(bfs_q) != 0:
+        curr = bfs_q.popleft()
+        if curr != None:
+            bfs_q.append(curr.left)
+            bfs_q.append(curr.right)
+    return ans
 
 
 if __name__ == '__main__':
