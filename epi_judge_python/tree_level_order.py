d90ed12aa6573a94f70c106f07a59298f4757aa5
bliutwo <bliutwo@cs.stanford.edu>
2021-03-16 21:35:42 -0700

    Revert to original 8.6

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -5,18 +5,8 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    result: List[List[int]] = []
-    if not tree:
-        return result
-
-    curr_depth_nodes = [tree]
-    while curr_depth_nodes:
-        result.append([curr.data for curr in curr_depth_nodes])
-        curr_depth_nodes = [
-            child for curr in curr_depth_nodes
-            for child in (curr.left, curr.right) if child
-        ]
-    return result
+    # TODO - you fill in here
+    return []
 
 
 if __name__ == '__main__':
