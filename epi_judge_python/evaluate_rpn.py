a2b405ead3bf1e1848d8169117c7704a92e1458e
bliutwo <bliutwo@cs.stanford.edu>
2021-03-17 19:16:37 -0700

    Revert "Add working solution 8.2"
    This reverts commit f15824f166185b6c975d48c80f991677511ed3ee.

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,15 +2,8 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    l = expression.split(',')
-    d = {'*': lambda y, x: x * y, '/': lambda y, x: x // y, '+': lambda y, x: x + y, '-': lambda y, x: x - y}
-    stack = []
-    for elem in l:
-        if elem in d:
-            stack.append(d[elem](int(stack.pop()), int(stack.pop())))
-        else:
-            stack.append(int(elem))
-    return stack[0]
+    # TODO - you fill in here.
+    return 0
 
 
 if __name__ == '__main__':
