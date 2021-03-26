aa9cae62875a69d9840360001cd5f5ef75b19828
bliutwo <bliutwo@cs.stanford.edu>
2021-03-25 19:10:51 -0700

    Add working solution 8.2

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack = []
+    l = expression.split(',')
+    d = {'/': lambda y, x: x // y, '*': lambda y, x: x * y, '+': lambda y, x: x + y, '-': lambda y, x: x - y}
+    for word in l:
+        if word in d:
+            stack.append(d[word](stack.pop(), stack.pop()))
+        else:
+            stack.append(int(word))
+    return stack[0]
 
 
 if __name__ == '__main__':
