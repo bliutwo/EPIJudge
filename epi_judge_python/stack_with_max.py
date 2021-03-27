ba4bf5dbb8fb512ea1f0ee7475cd1df87bd9370d
bliutwo <bliutwo@cs.stanford.edu>
2021-03-27 15:20:41 -0700

    Add working solution 8.1

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -1,23 +1,29 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import collections
+
 
 
 class Stack:
+    MaxAndValue = collections.namedtuple('MaxAndValue', ('max', 'value'))
+    def __init__(self):
+        self._stack = []
+
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return len(self._stack) == 0
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self._stack[-1].max
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self._stack.pop().value
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._stack.append(
+            self.MaxAndValue(x, x) if self.empty() else
+            self.MaxAndValue(max(x, self.max()), x)
+        )
+
 
 
 def stack_tester(ops):
